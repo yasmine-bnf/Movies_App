f540ad9e9c696233f9917239e662fcc5246e37fe
yasmine-bnf <140459355+yasmine-bnf@users.noreply.github.com>
2024-10-04 15:16:22 +0100

    finished project

---
+++ app/src/main/java/com/app/moviesapp/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.app.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
